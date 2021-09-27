a3bdf63a6d85ba4b6ef993bb84da1a0e56cc16dd
Viraj Hegshetye <virajhegshetye1@gmail.com>
2021-09-28 01:47:23 +0530

    added new posts code

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,15 +1,26 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class BlogController {
+
+    private PostRepository postRepository;
+
+    public BlogController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
     @RequestMapping("/")
     public String listPosts(ModelMap modelMap){
-        modelMap.put("title","Blog post 1");
-        modelMap.put("title2","blog post 2");
+        List<Post> posts=postRepository.getAllPosts();
+        modelMap.put("posts",posts);
         return "home";
     }
 }
